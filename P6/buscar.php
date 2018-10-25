7528fa324e83cc1971a839433d045101cbf57252
Alejandro Roca Vande Sype <31854308+AlexRoca95@users.noreply.github.com>
2018-10-25 19:43:54 +0200

    campos obligatorios en busqueda avanzada

--- P6/buscar.php
+++ P6/buscar.php
@@ -6,30 +6,30 @@
 	<title>PRETI</title>
 	
 	<?php
-		require_once("includes/estilos.inc");  # Contiene todos los enlaces con el css necesario para las paginas
+		require("includes/estilos.inc");  # Contiene todos los enlaces con el css necesario para las paginas
 	?>
 
 </head>
 <body>
 	
 	<?php
-		require_once("includes/cabecera.inc");  # Cabecera de la pagina con el logo, login y registro
+		require("includes/cabecera.inc");  # Cabecera de la pagina con el logo, login y registro
 	?>
 
 	<section id="busquedaAvanzada">
 		<fieldset class="marcoBusqueda">
 			<h2>Búsqueda Avanzada</h2>
 			<form action="resBuscar.php" method="GET">
-				<p><label><b>Título </b></label><input class="boxesAlbum" type="text" name="titulo" placeholder=" Amanecer..."></p>
-				<p><label><b>País </b></label><input class="boxesAlbum" type="text" name="pais" placeholder=" España..."></p>
-				<p><label><b>Fecha entre </b></label> <input class="boxesAlbum" type="date" name="fechaInicial"> <b> y </b> <input class="boxesAlbum" type="date" name="fechaFinal"></p>
+				<p><label><b>Título </b></label><input class="boxesAlbum" type="text" name="titulo" placeholder=" Amanecer..." required></p>
+				<p><label><b>País </b></label><input class="boxesAlbum" type="text" name="pais" placeholder=" España..."required></p>
+				<p><label><b>Fecha entre </b></label> <input class="boxesAlbum" type="date" name="fechaInicial" required> <b> y </b> <input class="boxesAlbum" type="date" name="fechaFinal" required></p>
 				<p><input id="buscarFotos" type="submit" value="Buscar Fotos"></p>
 			</form>
 		</fieldset>
 	</section>
 
 	<?php
-		require_once("includes/pie.inc");  # Pie de la pagina con el copyright
+		require("includes/pie.inc");  # Pie de la pagina con el copyright
 	?>
 	
 </body>

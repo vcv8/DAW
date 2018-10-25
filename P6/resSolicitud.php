f8b342f66a48cc6234d1105dcbbe90a97a9083a2
Alejandro Roca Vande Sype <31854308+AlexRoca95@users.noreply.github.com>
2018-10-25 17:36:55 +0200

    Calculo del coste de album

--- P6/resSolicitud.php
+++ P6/resSolicitud.php
@@ -17,18 +17,64 @@
 	?>
 
 	<section>
-		<h2>Confirmación de solicitud</h2>
+		<fieldset id="marcoResAlbum">
+			<h2>Confirmación de solicitud</h2>
 
-		<?php
+			<?php
 
-			$album = $_GET['album'];
+				$album = $_GET['album'];  	# Album Seleccionado
+				$copias = $_GET['copias'];	# Nº de copias del album
+				$paginas = 5; 				# Nº de paginas del album
+				$fotos = 8; 				# Nº de fotos del album
+				$resolucion = $_GET['resolucion']; # Resolucion del album
+				$tipoImpresion = $_GET['cimpresion']; 	# Tipo de impresion del album 
 
-			echo "<p>Se ha registrado su solicitud de impresión para el album <b>$album</b> <p>"
+				$total = 0;
+				$costePag = 0;
+				$costeImpresion = 0;
+				$costeResolucion = 0;
 
-		?>
-		<!--<p>Se ha registrado su solicitud de impresión para el álbum <b>Paisajes</b>.</p> -->
-		<p>El coste total de la operación son <b>13.40 €</b>.</p>
-		<a href="usuarioRegistrado.php">Volver a mi perfil.</a>
+				# Calculamos el precio por pagina
+				if ($paginas<5) {
+				    $costePag = 0.10;
+
+				} elseif ($paginas>=5 && $paginas<=10) {
+				    $costePag = 0.08;
+
+				} else { # Mas de 10 paginas
+				    $costePag = 0.07;
+				}
+
+				# Si la impresion a color añadimos el coste
+				if($tipoImpresion == "color")
+				{
+					$costeImpresion = 0.05;
+				}
+
+				if($resolucion >300)
+				{
+					$costeResolucion = 0.02;
+				}
+
+				#Calculo del precio final
+				$costeFinalPag = $costePag * $paginas; # Coste total por numero de paginas
+
+				$costeImpresion = $costeImpresion + $costeResolucion;
+				$costeFinalFotos = $costeImpresion * $fotos;
+
+				$total = ($costeFinalPag + $costeFinalFotos) * $copias;
+
+
+
+
+
+				echo "<p>Se ha registrado su solicitud de impresión para el album <b>$album</b> <p>";
+				echo "<p>El coste total de la operación son <b>$total €</b>.</p>"
+
+
+			?>
+			<a href="usuarioRegistrado.php">Volver a mi perfil.</a>
+		</fieldset>
 	</section>
 
 	<?php

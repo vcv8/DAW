873e58467db6bbf3938151264bf3cff17c7b67e0
vcv8 <vcv183@gmail.com>
2018-11-19 19:10:54 +0100

    includo con cookie de bienvenida

--- P8/about.php
+++ P8/about.php
@@ -10,14 +10,7 @@
 			exit;
 		}	 
 	}else{
-		$dia = date('d');
-		$mes = date('F');
-		$anyo = date('Y');
-		$hora = date('h');
-		$min = date('i');
-		$sec = date('s');
-		$pm = date('A');
-		setcookie("lasttime", "Bienvenido " . $_SESSION['usuario'] . ", no te veíamos desde el día $dia de $mes de $anyo a las $hora:$min:$sec $pm.", time() + (100 * 24 * 60 * 60));
+		require_once("includes/mBienvenida.inc");
 	}
 ?>
 <!DOCTYPE html>

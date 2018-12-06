8d29e178bd84e612c15d8116636bc8510aacfac5
Victor <vcv183@gmail.com>
2018-12-06 19:42:09 +0100

    camba estilo

--- P9/updateEstilo.php
+++ P9/updateEstilo.php
@@ -14,12 +14,19 @@
 			}
 
 			if($resUp){
+				$estilo = $mysqli->query("SELECT Nombre FROM estilos WHERE IdEstilo=$eNuevo");
+				$festilo = $estilo->fetch_assoc();
+
+				$cookie_name = "estilo";
+				$cookie_value = $festilo['Nombre']; # Almacenamos el nombre del estilo que tiene almacenado el usuario
+				setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+
 				#Redireccionamos de vuelta a la pagina de cambio de estilo con el estilo nuevo puesto y un mensaje
 				$host = $_SERVER['HTTP_HOST']; 
 				$uri  = rtrim(dirname($_SERVER[’PHP_SELF’]), '/\\'); 
 				$extra = 'P9/configurarEstilo.php?enuevo='. $eNuevo; 
 				header("Location: http://$host$uri/$extra");
-		exit;	
+				exit;	
 			}
 		}
 	}

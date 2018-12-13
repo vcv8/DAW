3546009a2cbb748e996098ea2a297a76a2186bd4
AlexRoca95 <alexdragonit.drag@hotmail.com>
2018-12-13 20:14:40 +0100

    Seleccion de foto sin terminar

--- P10/index.php
+++ P10/index.php
@@ -51,6 +51,78 @@
 	<section class="preview"> <!-- 5 Ultimas Imagenes -->
 
 		<?php
+
+			#Seleccion de foto por critico 
+			$ficheroFoto = file("./recursos/seleccionFotos.txt"); #Abrimos fichero
+			$numLineas = count($ficheroFoto); # Numero de lineas que tiene el fichero
+
+			#echo $numLineas;
+			if($ficheroFoto)
+			{
+				$numRandom = rand(0,$numLineas-1);
+				list($tituloFoto, $autor, $comentario) = explode("|", $ficheroFoto[$numRandom]); 
+
+				$sentencia3 = "SELECT * FROM fotos WHERE Titulo='$tituloFoto'";
+				$fotoFichero = $mysqli->query($sentencia3);
+
+				if(!$fotoFichero || $mysqli->errno) # errno devuelve el codigo de error de la ultima funcion ejecutada
+				{
+					die("Error: no se pudo realizar la consulta: " . $mysqli->error);
+				}
+
+				$filaFotoFichero = $fotoFichero->fetch_assoc();
+
+				$idPais2 =  $filaFotoFichero['Pais'];
+
+				if($idPais2!=NULL)
+				{
+					$sentencia4 = "SELECT * FROM paises WHERE IdPais=$idPais2";
+					$nomPaisesSelec = $mysqli->query($sentencia4);  # Devuelve un objeto con el pais que tenga el mismo ID
+
+					if(!$nomPaisesSelec || $mysqli->errno) # errno devuelve el codigo de error de la ultima funcion ejecutada
+					{
+						die("Error: no se pudo realizar la consulta: " . $mysqli->error);
+					}
+						
+					$filaPais =  $nomPaisesSelec->fetch_assoc();
+				}
+				
+				
+
+		?>
+
+		<article>
+			<a href=<?php echo "'detalleFoto.php?id_foto=" . $filaFotoFichero['IdFoto'] . "' >"; ?> 
+				<figure>
+					<img <?php echo "src='" . $filaFotoFichero['Fichero'] . "'" ?> class="prov">
+					<figcaption class="top-right">
+						<div class="imgResume">
+							<p><b><?php echo $filaFotoFichero['Titulo']; ?></b></p>
+							<p>
+							<?php 
+								if($filaFotoFichero['Fecha']!=null)
+								{
+									echo str_replace('-', '/', date('d/m/Y', strtotime($filaFotoFichero['Fecha']))); 
+								}
+							?>
+							</p>
+							<p id="irPais">
+								<?php 
+									if( $idPais2 !=NULL )
+									{
+										echo $filaPais['NomPais'];
+									}
+								?>	
+							</p>
+						</div>
+					</figcaption>
+				</figure>
+			</a>
+		</article>
+
+		<?php 
+
+			}
 			/*$esChars = $mysqli->query("SET NAMES 'utf8'");*/
 			$sentencia1 = "SELECT * FROM fotos ORDER BY FRegistro DESC LIMIT 5";
 			$fotos = $mysqli->query($sentencia1);  # Devuelve un objeto con todas las fotos
